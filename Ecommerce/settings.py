b4026dd0d2a618540de14716dca2bf642c8e8eeb
alireza <alirezahn1@yahoo.com>
2022-02-25 00:15:11 +0330

    feat implement user model

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'products',
+    'orders',
+    'customers',
 ]
 
 MIDDLEWARE = [
@@ -117,8 +121,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
+# Media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'core.User'
--- core/models.py
+++ core/models.py
@@ -46,3 +46,20 @@ class BaseModel(models.Model):
     def activate(self):
         self.is_active = True
         self.save()
+
+
+from django.contrib.auth.models import AbstractUser, UserManager
+
+class MyUserManager(UserManager):
+
+    def create_superuser(self, username=None, email=None, password=None, **extra_fields):
+        username = extra_fields['phone']
+        return super().create_superuser(username, email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    phone = models.CharField(max_length=15, unique=True)
+
+    USERNAME_FIELD = 'phone'
+
+    objects = MyUserManager()
