1189b183c79c6861630d056ef49134be66f64cf9
alireza <alirezahn1@yahoo.com>
2022-02-26 02:29:57 +0330

    feat category admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Discount)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('name', 'slug')
+	prepopulated_fields = {'slug': ('name',)}
+
