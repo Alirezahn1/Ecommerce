fc84e973ff1d7c5202c430b689f0def341d378d6
alireza <alirezahn1@yahoo.com>
2022-02-26 02:38:13 +0330

    feat order model

--- orders/models.py
+++ orders/models.py
@@ -7,3 +7,12 @@ from products.models import Product, AbstractDiscount
 
 class OffCode(AbstractDiscount):
     code = models.CharField(max_length=15, unique=True)
+
+
+class Order(BaseModel):
+    customer = models.ForeignKey(Customer,on_delete=models.RESTRICT)
+    address = models.ForeignKey(Address, on_delete=models.RESTRICT)
+    off_code = models.ForeignKey(OffCode, on_delete=models.SET_NULL, null=True, blank=True)
+
+    class Meta:
+        unique_together = [['off_code', 'customer']]
