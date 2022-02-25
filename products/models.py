fc514a5c74764a30b0786cdb88a3e46e1313a68c
alireza <alirezahn1@yahoo.com>
2022-02-26 02:25:43 +0330

    feat base discount model

---
+++ products/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.urls import reverse
+
+from core.models import BaseModel
+# Create your models here.
+
+class AbstractDiscount(BaseModel):
+    value = models.PositiveIntegerField(null=False)
+    type = models.CharField(max_length=10, choices=[('price', 'Price'), ('percent', 'Percent')], null=False)
+    max_price = models.PositiveIntegerField(null=True, blank=True)
+
+    def profit_value(self, price: int):
+        """
+        Calculate and Return the profit of the discount
+        :param price: int (item value)
+        :return: profit
+        """
+        if self.type == 'price':
+            return min(self.value, price)
+        else:  # percent
+            raw_profit = int((self.value / 100) * price)
+            return int(min(raw_profit, int(self.max_price))) if self.max_price else raw_profit
+
+    class Meta:
+        abstract = True
+
+
+class Discount(AbstractDiscount):
+    pass
+
+
+
+
