cf304eafc1dc24320b56eab572eabf8b058f77d0
alireza <alirezahn1@yahoo.com>
2022-02-27 01:03:38 +0330

    fix product view

--- products/urls.py
+++ products/urls.py
@@ -1,14 +1,15 @@
 
 from django.urls import path
-app_name = 'product'
+app_name = 'products'
 
 from .views import *
+
 urlpatterns = [
     path('',HomeView.as_view(),name='home'),
-    path('about',AboutView.as_view(),name='about'),
-    path('product',product,name='product'),
-    path('why',WhyView.as_view(),name='why'),
-    path('contact_us',ContactUsView.as_view(),name='contact_us'),
-    path('<slug:slug>/', product_detail, name='product_detail'),
-    path('category/<slug:slug>/', product, name='category_filter'),
+    path('about/',AboutView.as_view(),name='about'),
+    path('product/',ProductView.as_view(),name='product'),
+    path('why/',WhyView.as_view(),name='why'),
+    path('contact_us/',ContactUsView.as_view(),name='contact_us'),
+    path('<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('category/<slug:slug>/', ProductView.as_view(), name='category_filter'),
 ]
