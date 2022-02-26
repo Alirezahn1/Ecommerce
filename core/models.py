28d22aeff424efcf25ff1616f67312c9842af788
alireza <alirezahn1@yahoo.com>
2022-02-26 12:05:10 +0330

    fix user model

--- core/models.py
+++ core/models.py
@@ -52,15 +52,13 @@ from django.contrib.auth.models import AbstractUser, UserManager
 
 class MyUserManager(UserManager):
 
+
     def create_superuser(self, username=None, email=None, password=None, **extra_fields):
         username = extra_fields['phone']
         return super().create_superuser(username, email, password, **extra_fields)
 
 
 class User(AbstractUser):
-    phone = models.CharField(max_length=15, unique=True)
-
-    USERNAME_FIELD = 'phone'
-    username = None
-
     objects = MyUserManager()
+    phone = models.CharField(max_length=13, unique=True)
+    USERNAME_FIELD = 'phone'
