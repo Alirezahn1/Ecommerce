9aedcf4fc7116847a3087e9b7f47456d8118abcd
alireza <alirezahn1@yahoo.com>
2022-03-01 13:37:06 +0330

    fix register user view

--- customers/views.py
+++ customers/views.py
@@ -16,24 +16,26 @@ class UserRegisterView(View):
         if request.method == 'POST':
             form = UserCreationForm(request.POST)
             if form.is_valid():
-                cd = form.cleaned_data
-                email= cd['email']
-                phone= cd['phone']
-                first_name= cd['first_name']
-                password1= cd['password1']
-                last_name= cd['last_name']
-                password2= cd['password2']
-                if not password1==password2 :
-                    messages.error(request, 'passwords must match','error')
-                    return redirect('customers:register')
-                user = User.objects.create(email=email,phone=phone, first_name=first_name, last_name=last_name,password=password1)
+                # cd = form.cleaned_data
+                # email= cd['email']
+                # phone= cd['phone']
+                # first_name= cd['first_name']
+                # password1= cd['password1']
+                # last_name= cd['last_name']
+                # password2= cd['password2']
+                #
+                # user = User.objects.create(email=email,phone=phone, first_name=first_name, last_name=last_name,password=password1)
+                # user.save()
+
+                user = form.save(commit=False)
                 user.save()
                 customer = Customer.objects.create(user=user)
                 customer.save()
-
                 messages.success(request, 'successfully registered', 'success')
                 return redirect('customers:login')
-            return redirect('products:home')
+            else:
+                messages.error(request,form.errors,'danger')
+                return redirect('customers:register')
     def get(self,request):
         form = UserCreationForm()
         return render(request, 'customer/register.html', {'form': form})
@@ -43,6 +45,7 @@ class UserLoginView(View):
     def post(self,request):
         if request.method == 'POST':
             form = UserLoginForm(request.POST)
+
             if form.is_valid():
                 cd = form.cleaned_data
                 user = authenticate(request, phone=cd['phone'], password=cd['password'])
@@ -51,9 +54,13 @@ class UserLoginView(View):
                     messages.success(request, 'you logged in successfully', 'success')
                     return redirect('products:home')
                 else:
+
                     messages.error(request, 'phone or password is wrong', 'danger')
+                    return redirect('customers:login')
     def get(self,request):
+
         form = UserLoginForm()
+
         return render(request, 'customer/login.html', {'form': form})
 
 
